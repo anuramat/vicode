80b7c230b04856eccfe1837ed8baf4883aa9de48
Arsen Nuramatov <x@ctrl.sn>
2026-04-03 03:59:59 +0200

    chore(commands): docstrings

--- src/tui/command.rs
+++ src/tui/command.rs
@@ -20,19 +20,33 @@ serde_plain::derive_fromstr_from_deserialize!(CommandName);
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, EnumIter, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum CommandName {
+    /// select next assistant in the currently selected tab
     AssistantNext,
+    /// select previous assistant in the currently selected tab
     AssistantPrev,
+    /// switch to cmdline
     CmdlineEnter,
+    /// compact the conversation
     Compact,
+    /// cancel completion and hide the menu
     CompletionCancel,
+    /// select next completion result
     CompletionNext,
+    /// select previous completion result
     CompletionPrev,
+    /// exit insert mode/cmdline
     InputExit,
+    /// submit the message/command
     InputSubmit,
+    /// switch to insert mode
     InsertEnter,
+    /// paste args into focused input
     InsertPaste,
+    /// remove last message
     MsgUndo,
+    /// remove messages starting from last user message
     MsgUndoUser,
+    /// quit the app
     Quit,
     ScrollBottom,
     ScrollHalfPageDown,
@@ -44,19 +58,33 @@ pub enum CommandName {
     ScrollPageUp,
     ScrollPrevElement,
     ScrollTop,
+    /// set multiplier for the next prompt
     SetMultiplier,
+    /// delete the currently selected tab
     TabDelete,
+    /// duplicate the currently selected tab
     TabDuplicate,
+    /// open new tab
     TabNew,
+    /// switch to the next tab
     TabNext,
+    /// switch to the previous tab
     TabPrev,
+    /// select tab by index
     TabSelect,
+    /// show/hide developer messages
     ToggleDeveloper,
+    /// toggle markdown rendering
     ToggleMarkdown,
+    /// show/hide CoT
     ToggleReasoning,
+    /// show/hide tab pane
     ToggleTabs,
+    /// show/hide tool calls
     ToggleTools,
+    /// abort current turn
     TurnAbort,
+    /// retry turn/compact
     TurnRetry,
     /// dummy command to unmap keys
     #[serde(alias = "")]
